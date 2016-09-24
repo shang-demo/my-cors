0514285d1cca637fd79ac10f399bbdcf537e3ea9
xinshangshangxin <xinshangshangxin@gmail.com>
2016-09-24 22:28:18 +0800

    feat: 支持大部分URL

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ proxy.on('proxyReq', function(proxyReq, req, res, options) {
 });
 
 proxy.on('proxyRes', function(proxyRes, req, res) {
-  if(req.query.debug
+  if(proxyRes.statusCode === 301 || proxyRes.statusCode === 302) {
+    proxyRes.headers.location = '/url/' + encodeURIComponent(proxyRes.headers.location);
+  }
+
+  if(!req.query.disableDebug
     && proxyRes.headers
     && proxyRes.headers['content-type']
     && /text\/html/.test(proxyRes.headers['content-type'])) {
@@ -57,64 +61,53 @@ proxy.on('proxyRes', function(proxyRes, req, res) {
   }
 });
 
+
+// /url/:url
+
 var server = http.createServer(function(req, res) {
   cors(req, res, function() {
 
-    var queryObject = url.parse(req.url, true).query;
-    req.query = queryObject;
-
-    if(!queryObject.proxyTo) {
-      var redirectTo = parseReferToRedirect(req);
-      if(!redirectTo) {
-        res.writeHead(400, {
-          'Content-Type': 'text/json'
-        });
-        return res.end(JSON.stringify({
-          code: 400,
-          msg: 'need query proxyTo'
-        }));
-      }
-
-
-      if(req.method === 'POST') {
-        queryObject.proxyTo = redirectTo.uri;
-      }
-      else {
-        res.writeHead(301, {
-          Location: redirectTo.prefix + encodeURIComponent(redirectTo.uri)
-        });
-        res.end();
-        return;
-      }
+    if(req.url === '/') {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      return res.end(getFirstPageHtml());
     }
 
-    if(queryObject.debug) {
+    var redirectAbsoluteUrl = getStartUrl(req, res);
+    console.log('\n', 'startUrl', redirectAbsoluteUrl);
+    if(!redirectAbsoluteUrl || !/^http/.test(redirectAbsoluteUrl)) {
+      res.writeHead(400, {
+        'Content-Type': 'text/json'
+      });
+      return res.end(JSON.stringify({
+        code: 400,
+        msg: 'need start url'
+      }));
+    }
+
+
+    // reset url to prevent node-http-proxy set path
+    req.url = '';
+
+    if(!req.query.disableDebug) {
       req.headers = req.headers || {};
       req.headers['accept-encoding'] = null;
     }
 
-    queryObject.proxyTo = /^http/.test(queryObject.proxyTo) ? queryObject.proxyTo : 'http://' + queryObject.proxyTo;
-
-    queryObject.proxyTo = encodeURI(queryObject.proxyTo);
-    // reset url to prevent node-http-proxy set path
-    req.url = '';
-
-    console.info(req.method, queryObject.proxyTo);
-    if(!queryObject.type) {
-      return proxy.web(req, res, {
-        target: queryObject.proxyTo,
-        changeOrigin: true,
-        secure: false
-      });
-    }
-
-    // TODO: 根据type 加入代理
     return proxy.web(req, res, {
-      target: 'http://120.26.89.195:80',
+      target: redirectAbsoluteUrl,
       changeOrigin: true,
-      secure: false,
-      proxyTo: queryObject.proxyTo
+      secure: false
     });
+
+    // // TODO: 加入代理
+    // return proxy.web(req, res, {
+    //   target: 'http://120.26.89.195:80',
+    //   changeOrigin: true,
+    //   secure: false,
+    //   proxyTo: queryObject.proxyTo
+    // });
   });
 });
 
@@ -126,20 +119,31 @@ proxy.on('error', function(err, req, res) {
 });
 
 
-function parseReferToRedirect(req) {
-  var referUrlParse = url.parse((req.headers || {}).referer || '', true);
-  var refererObj = referUrlParse.query;
+function getRedirectAbsoluteUrl(req, startUrl) {
+  if(!startUrl) {
+    console.log('no startUrl, try to get from req.headers.referer');
 
-  refererObj.proxyTo = /^http/.test(refererObj.proxyTo) ? refererObj.proxyTo : 'http://' + refererObj.proxyTo;
+    var referer = (req.headers || {}).referer || '';
+    if(!referer) {
+      console.log('no startUrl from url, cookie and req.headers.referer');
+      return false;
+    }
 
-  var originObj = url.parse(refererObj.proxyTo || '', true);
-  if(!originObj.protocol || !originObj.host || originObj.host === 'undefined' || !req.url) {
-    return false;
+    startUrl = referer;
+
+    // var referUrlParse = url.parse(referer, true);
+    // var refererObj = referUrlParse.query;
+    // startUrl = referUrlParse.protocol + '//' + referUrlParse.host + '?' + (refererObj.disableDebig ? 'disableDebug=1&' : '');
   }
-  return {
-    prefix: referUrlParse.protocol + '//' + referUrlParse.host + '?' + (refererObj.debug ? 'debug=1&' : '') + 'proxyTo=',
-    uri: (originObj.protocol + '//' + originObj.host + req.url)
-  };
+
+  var redirectUri = req.url;
+  if(/\/url\/([^\/]+)/.test(redirectUri)) {
+    redirectUri = redirectUri.replace(/\/url\/([^\/]+)\/?/, '');
+  }
+
+  console.log('redirectUri', redirectUri, 'startUrl', startUrl);
+
+  return getAbsUrl(redirectUri, startUrl);
 }
 
 function modifyHtml(str) {
@@ -159,6 +163,104 @@ function modifyHtml(str) {
     console.log('not find html or body');
   }
   return str;
+}
+
+function getAbsUrl(url, base) {
+  if(!base) {
+    return url;
+  }
+
+  if(!url) {
+    return base;
+  } else if(/^[a-z][-+\.a-z0-9]*:/i.test(url)) {
+    // The scheme actually could contain any kind of alphanumerical unicode
+    // character, but JavaScript regular expressions don't support unicode
+    // character classes. Maybe /^[^:]+:/ or even /^.*:/ would be sufficient?
+    return url;
+  } else if(url.slice(0, 2) === '//') {
+    return /^[^:]+:/.exec(base)[0] + url;
+  }
+
+  var ch = url.charAt(0);
+  if(ch === '/') {
+    if(/^file:/i.test(base)) {
+      // file scheme has no hostname
+      return 'file://' + url;
+    } else {
+      return /^[^:]+:\/*[^\/]+/i.exec(base)[0] + url;
+    }
+  } else if(ch === '#') {
+    // assume "#" only occures at the end indicating the fragment
+    return base.replace(/#.*$/, '') + url;
+  } else if(ch === '?') {
+    // assume "?" and "#" only occure at the end indicating the query
+    // and the fragment
+    return base.replace(/[\?#].*$/, '') + url;
+  } else {
+    var base, path;
+    if(/^file:/i.test(base)) {
+      base = "file://";
+      path = base.replace(/^file:\/{0,2}/i, '');
+    } else {
+      var match = /^([^:]+:\/*[^\/]+)(\/.*?)?(\?.*?)?(#.*)?$/.exec(base);
+      base = match[1];
+      path = match[2] || "/";
+    }
+
+    path = path.split("/");
+    path.pop();
+    if(path.length === 0) {
+      // Ensure leading "/". Of course this is only valid on
+      // unix like filesystems. More magic would be needed to
+      // support other filesystems.
+      path.push("");
+    }
+    path.push(url);
+    return base + path.join("/");
+  }
+}
+
+function getStartUrl(req, res) {
+  req.cookies = {};
+  (req.headers.cookie && req.headers.cookie.split(';') || []).reduce(function(oldValue, Cookie) {
+    var parts = Cookie.split('=');
+    oldValue[parts[0].trim()] = decodeURIComponent(( parts[1] || '' ).trim());
+    return oldValue;
+  }, req.cookies);
+
+  req.query = url.parse(req.url, true).query;
+
+  var startUrl = '';
+  if(/\/url\/([^\/]+)/.test(req.url)) {
+    var u = decodeURIComponent(RegExp.$1);
+    if(/^http/.test(u)) {
+      console.log('use queryString startUrl', u);
+      startUrl = u;
+    }
+  }
+
+
+  if(!startUrl) {
+    console.log('use cookie startUrl', decodeURIComponent(req.cookies.startUrl || ''));
+    startUrl = decodeURIComponent(req.cookies.startUrl || '');
+  }
+
+  var expires = new Date();
+  expires.setDate(expires.getDate() + 1);
+  res.setHeader('Set-Cookie', ['startUrl=' + encodeURIComponent(startUrl) + '; Expires=' + expires.toISOString() + '; path=/; HttpOnly']);
+
+
+  var redirectUri = req.url.replace(/.*\/url\//, '');
+  if(/^http/.test(redirectUri)) {
+    redirectUri = redirectUri.replace(/^[^\/]*/, '');
+  }
+  console.log('redirectUri', redirectUri);
+  return getAbsUrl(redirectUri, startUrl);
+}
+
+function getFirstPageHtml() {
+  return '<!DOCTYPE html><html><head><title>cors</title><style type="text/css">button{height:50px;display:inline-block;font-weight:400;text-align:center;cursor:pointer;background-image:none;border:1px solid transparent;white-space:nowrap;padding:6px 12px;font-size:14px;line-height:1.42857143;border-radius:4px;}button:hover{background-color:#e7e4e4;}input{height:48px;width:200px;font-size:26px;color:black;border-radius:3px;border:none;background-color:#CCCCCC;padding-left:6px;outline:none;vertical-align:bottom;}input[type="checkbox"]{width:20px;vertical-align:middle;display:inline-block;background-color:white;}#url{width:800px;font-size:25px;}</style></head><body><input type="url"id="url"><button id="redirect">redirect</button><script type="text/javascript">var getAbsoluteUrl=(function(){var a;return function(url){if(!a){a=document.createElement(\'a\');}'+
+    'a.href=url;return a.href;};})();var oRedirect=document.getElementById(\'redirect\');var oUrl=document.getElementById(\'url\');oRedirect.onclick=function(){if(!oUrl.value){alert(\'need redirect url!!!\');return;}window.location.href=getAbsoluteUrl(\'/url/\'+encodeURIComponent(oUrl.value));};</script></body></html>';;
 }
 
 
